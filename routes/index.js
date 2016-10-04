d2b52d281aae48bd752a5600dad86e6861853e14
Orlando Castillo <orlandoc1618@gmail.com>
2016-10-04 11:35:02 -0700

    add challenge expiration

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const contracts = {
   registrarAddress: Config['registrarAddress'],
   registryAddress: Config['registryAddress']
 };
+
 const sdk = new Provider(Config['rpcUrl'], 'consumer', null, contracts);
+const recentChallenges = {};
 
 module.exports = (app) => {
   app.route('/')
@@ -20,21 +22,28 @@ module.exports = (app) => {
         return res.status(400).send({reason: "Invalid URN format"});
       } else {
         return sdk.getThing(identity)
-        .then(() => res.send({challenge: Utils.generateChallenge(protocol)}))
+        .then((thing) => {
+          const challenge = Utils.generateChallenge(protocol);
+          const fiveMinutes = 5*60*1000; //ms
+          recentChallenges[challenge] = true;
+          setTimeout(() => delete recentChallenges[challenge], fiveMinutes);
+          res.send({challenge: Utils.generateChallenge(protocol), thing});
+        })
         .catch(() => res.status(400).send({reason: "Item could not be found"}));
       }
     });
 
   app.route('/verifyChallenge')
     .post((req, res) => {
-      const { identity, message, signature } = req.body;
+      const { identity, challenge, signature } = req.body;
       const { valid } = Utils.parseURN(identity);
+      const isRecentChallenge = isRecentChallenge[challenge];
 
       if(!valid) {
         return res.status(400).send({reason: "Invalid URN format"});
       } else {
-        return sdk.verifyIdentity(identity, message, signature)
-        .then(verified => res.send({verified}))
+        return sdk.verifyIdentity(identity, challenge, signature)
+        .then(verified => res.send({verified: verified && isRecentChallenge}))
         .catch(() => res.status(500).send({reason: "Internal Server Error"}));
       }
     });
