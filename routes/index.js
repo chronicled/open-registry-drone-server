73337e55a08b77816e10c0a087fe6f9e74b52a85
Orlando Castillo <orlandoc1618@gmail.com>
2016-10-04 12:34:38 -0700

    add identity validation

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const Provider = require('open-registry-sdk');
+const _ = require('lodash');
 const Config = require('../config.json');
 const Utils = require('../utils');
 const contracts = {
@@ -14,37 +15,40 @@ module.exports = (app) => {
     .get((req, res) => res.send(require('../package.json')));
 
   app.route('/requestChallenge')
-    .post((req, res) => {
-      const { identity } = req.body;
-      const { valid, protocol } = Utils.parseURN(identity);
-
-      if (!valid) {
-        return res.status(400).send({reason: "Invalid URN format"});
-      } else {
-        return sdk.getThing(identity)
-        .then((thing) => {
-          const challenge = Utils.generateChallenge(protocol);
-          const fiveMinutes = 5*60*1000; //ms
-          recentChallenges[challenge] = true;
-          setTimeout(() => delete recentChallenges[challenge], fiveMinutes);
-          res.send({challenge: Utils.generateChallenge(protocol), thing});
-        })
-        .catch(() => res.status(400).send({reason: "Item could not be found"}));
-      }
+    .post(parseIdentity, (req, res) => {
+      const { identity, parsedIdentity: {publicKey, protocol} } = req.body
+      return sdk.getThing(identity)
+      .then((thing) => {
+        const challenge = Utils.generateChallenge(protocol);
+        const fiveMinutes = 5*60*1000; //ms
+        recentChallenges[challenge] = true;
+        setTimeout(() => delete recentChallenges[challenge], fiveMinutes);
+        res.send({challenge , thing});
+      })
+      .catch(() => res.status(400).send({reason: "Item could not be found"}));
     });
 
   app.route('/verifyChallenge')
-    .post((req, res) => {
+    .post(parseIdentity, (req, res) => {
       const { identity, challenge, signature } = req.body;
-      const { valid } = Utils.parseURN(identity);
       const isRecentChallenge = isRecentChallenge[challenge];
-
-      if(!valid) {
-        return res.status(400).send({reason: "Invalid URN format"});
-      } else {
-        return sdk.verifyIdentity(identity, challenge, signature)
-        .then(verified => res.send({verified: verified && isRecentChallenge}))
-        .catch(() => res.status(500).send({reason: "Internal Server Error"}));
-      }
+      return sdk.verifyIdentity(identity, challenge, signature)
+      .then(verified => res.send({verified: verified && isRecentChallenge}))
+      .catch(() => res.status(500).send({reason: "Internal Server Error"}));
     });
+
+  function parseIdentity(req, res, next) {
+    const { identity } = req.body;
+    const { valid, protocol, publicKey } = Utils.parseURN(identity); 
+    const isRecongizedProtocol = _.values(Utils.protocols).some(p => p === protocol);
+    if (!valid) {
+      res.status(400).send({reason: "Invalid URN format"});
+    } else if (!isRecongizedProtocol) {
+      res.status(400).send({reason: "Public key protocol not supported"})
+    } else {
+      req.body.parsedIdentity = {valid, protocol, publicKey};
+      next();
+    }
+  }
+
 };
